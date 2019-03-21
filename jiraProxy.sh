ce3865dc36bddadb35382fd7a4f002b2f106df43
Eddie Webb <ollitech@gmail.com>
2019-03-21 11:03:35 -0400

    test script allows deployments

--- jiraProxy.sh
+++ jiraProxy.sh
@@ -6,7 +6,7 @@ curl \
 -w "%{http_code}" \
 -H "Content-Type: application/json" \
 -H "Accept: application/json" \
--X POST "https://circleci.com/api/v1.1/project/github/eddiewebb/circleci-samples/jira/build?circle-token=${CIRCLECI_API_TOKEN}" --data @$1
+-X POST "https://circleci.com/api/v1.1/project/github/eddiewebb/circleci-samples/jira/${1}?circle-token=${CIRCLECI_API_TOKEN}" --data @$2
 
 
 
